24e919cdd6997d2cc97fb7e44c88e91f158ef176
Савелий Савенок <pavel_sa@holdingbp.ru>
2024-11-19 20:36:05 +0300

    fix: fixed debug secret

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ App({
     TOTPS: localStorage.getItem('TOTPs') || []
   },
   onCreate(options) {
-     localStorage.setItem('TOTPs', [
-      new TOTP('JBSWY3DPEHPK3PXPQ', 'totp.danhersam.com', 'Iasd')
-    ])
+    //  localStorage.setItem('TOTPs', [
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I1', 6, 30, 0, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I2', 6, 30, 5, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I3', 6, 30, -5, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I4', 6, 30, 0, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I5', 6, 30, 0, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I6', 6, 30, 0, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I7', 6, 30, 0, 'SHA-1'),
+    //   new TOTP('JBSWY3DPEHPK3PXP', 'totp.danhersam.com', 'I8', 6, 30, 0, 'SHA-1')
+    // ])
   },
 
   onDestroy(options) {
